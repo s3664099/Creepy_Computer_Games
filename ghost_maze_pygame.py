c754bc3c89d8599a37a5c11249bcfdc97097be05
Dave Sarkies <s3664099@student.rmit.edu.au>
2023-10-22 23:56:40 +1100

    Got the drawing of the screen correct

--- ghost_maze_pygame.py
+++ ghost_maze_pygame.py
@@ -11,6 +11,7 @@ def main(display_grid):
 
     # Set up the colors
     white = (255, 255, 255)
+    red = (255,0,0)
 
 
     # Main loop
@@ -22,6 +23,7 @@ def main(display_grid):
         size = 20
         size_start = 0
 
+        #Moves through the grid
         for x in display_grid:
 
             x_position = x_pos_start
@@ -30,24 +32,36 @@ def main(display_grid):
 
             for y in x:
 
+
+                #Checks whether there is an exit, and if so changes the colour to red
+                colour = white
+
+                if y =="9":
+                    colour = red
+                    y = "#" 
+
                 polygon_size = size/2
 
+                #Checks whether there is a wall
                 if y == "#":
-                    pygame.draw.rect(screen,white,(x_position,y_position,size,size))
 
+                    #Draws the wall
+                    pygame.draw.rect(screen,colour,(x_position,y_position,size,size))
+
+                    #Checks which position the wall is on, and then draws the appropriate polygon
                     if pos == 0:
 
                         if size_start == 0:
-                            pygame.draw.polygon(screen,white, ((x_position+size,y_position),(x_position+size+polygon_size,y_position+polygon_size),(x_position+size,y_position+size)))
+                            pygame.draw.polygon(screen,colour, ((x_position+size,y_position),(x_position+size+polygon_size,y_position+polygon_size),(x_position+size,y_position+size)))
                         else:
-                            pygame.draw.polygon(screen,white, ((x_position+size,y_position),(x_position+size+polygon_size,y_position+(polygon_size/2)),(x_position+size+polygon_size,y_position+(polygon_size)),(x_position+size,y_position+size)))
+                            pygame.draw.polygon(screen,colour, ((x_position+size,y_position),(x_position+size+(polygon_size/2),y_position+(polygon_size/2)),(x_position+size+(polygon_size/2),y_position+(size/2+polygon_size/2)),(x_position+size,y_position+size)))
 
                     if pos == 2:
 
                         if size_start == 0:
-                            pygame.draw.polygon(screen,white, ((x_position,y_position),(x_position-polygon_size,y_position+polygon_size),(x_position,y_position+size)))
+                            pygame.draw.polygon(screen,colour, ((x_position,y_position),(x_position-polygon_size,y_position+polygon_size),(x_position,y_position+size)))
                         else:
-                            pygame.draw.polygon(screen,white, ((x_position,y_position),(x_position-polygon_size,y_position+(polygon_size/2)),(x_position-polygon_size,y_position+(polygon_size)),(x_position,y_position+size)))
+                            pygame.draw.polygon(screen,colour, ((x_position,y_position),(x_position-(polygon_size/2),y_position+(polygon_size/2)),(x_position-(polygon_size/2),y_position+(size/2+polygon_size/2)),(x_position,y_position+size)))
 
                 pos+=1
 
@@ -60,42 +74,7 @@ def main(display_grid):
 
         pygame.display.flip()
 
-
-"""
-        if (display_grid[0][0] == "#"):
-            pygame.draw.rect(screen, white, (375,300,20,20))
-            pygame.draw.polygon(screen,white,((395,300),(405,310),(395,320)))
-        if (display_grid[0][1] == "#"):
-            pygame.draw.rect(screen, white, (395,300,20,20))
-        if (display_grid[0][2] == "#"):
-            pygame.draw.rect(screen, white, (415,300,20,20))
-            pygame.draw.polygon(screen, white, ((415,300),(405,310),(415,320)))
-        if (display_grid[1][0] == "#"):
-            pygame.draw.rect(screen,white, (345,290,40,40))
-            pygame.draw.polygon(screen,white,((385,290),(395,300),(395,320),(385,330)))
-        if (display_grid[1][1] == "#"):
-            pygame.draw.rect(screen,white, (385,290,40,40))
-        if (display_grid[1][2] == "#"):
-            pygame.draw.rect(screen,white, (425,290,40,40))
-            pygame.draw.polygon(screen,white, ((425,290),(415,300),(415,320),(425,330)))
-        if (display_grid[2][0] == "#"):
-            pygame.draw.rect(screen,white, (285,270,80,80))
-            pygame.draw.polygon(screen,white, ((365,270),(385,290),(385,330),(365,350)))           
-        if (display_grid[2][1] == "#"):
-            pygame.draw.rect(screen,white, (365,270,80,80))
-        if (display_grid[2][2] == "#"):
-            pygame.draw.rect(screen,white, (445,270,80,80))
-            pygame.draw.polygon(screen,white, ((445,270),(425,290),(425,330),(445,350)))
-"""
-
-def draw_square(screen,color,x,y,z):
-
-    pygame.draw.line(screen,color,(x,z),(y,z))
-    pygame.draw.line(screen,color,(x,z),(x,y))
-    pygame.draw.line(screen,color,(x,y),(y,y))
-    pygame.draw.line(screen,color,(y,y),(y,z))
-
-display_grid = [['#','','#'],['#','','#'],['#','','#']]
+display_grid = [['#','','#'],['9','','#'],['#','','#']]
 
 main(display_grid)
 
